1c0d407ba4caa0687e6fa88be1ca7e9e3dbce43f
Daniele Rondina <geaaru@macaronios.org>
2026-08-10 17:34:27 +0200

    Prepare v0.12.0

--- pkg/specs/config.go
+++ pkg/specs/config.go
@@ -13,7 +13,7 @@ import (
 const (
 	SSH_COMPOSE_CONFIGNAME = ".ssh-compose"
 	SSH_COMPOSE_ENV_PREFIX = "ssh_COMPOSE"
-	SSH_COMPOSE_VERSION    = `0.11.0`
+	SSH_COMPOSE_VERSION    = `0.12.0`
 )
 
 type SshComposeConfig struct {
