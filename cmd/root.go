60db8900ffa73f7ac920bdb2b0c63423d7d34b11
Daniele Rondina <geaaru@macaronios.org>
2026-08-13 18:44:46 +0200

    Add globals flags --encrypted-secrets, --render-secrets

--- cmd/root.go
+++ cmd/root.go
@@ -78,12 +78,15 @@ func initCommand(rootCmd *cobra.Command, config *specs.SshComposeConfig) {
 	var pflags = rootCmd.PersistentFlags()
 
 	pflags.StringP("config", "c", "", "SSH Compose configuration file")
+	pflags.String("render-secrets", "", "Override render secrets file.")
 	pflags.String("render-values", "", "Override render values file.")
 	pflags.String("render-default", "", "Override render default file.")
 	pflags.Bool("cmds-output", config.Viper.GetBool("logging.cmds_output"),
 		"Show hooks commands output or not.")
 	pflags.BoolP("debug", "d", config.Viper.GetBool("general.debug"),
 		"Enable debug output.")
+	pflags.Bool("encrypted-secrets", config.Viper.GetBool("security.encrypted_secrets"),
+		"Override the security.encrypted_secrets option.")
 	pflags.String("keyfile", "", "Overrife keyfile path for decryption.")
 	pflags.String("key", "", "Overrife key for decryption.")
 	pflags.Int("concurrency", runtime.NumCPU(), "Concurrency")
@@ -91,11 +94,13 @@ func initCommand(rootCmd *cobra.Command, config *specs.SshComposeConfig) {
 	config.Viper.BindPFlag("config", pflags.Lookup("config"))
 	config.Viper.BindPFlag("render_default_file", pflags.Lookup("render-default"))
 	config.Viper.BindPFlag("render_values_file", pflags.Lookup("render-values"))
+	config.Viper.BindPFlag("render_secrets_file", pflags.Lookup("render-secrets"))
 	config.Viper.BindPFlag("general.debug", pflags.Lookup("debug"))
 	config.Viper.BindPFlag("general.concurrency", pflags.Lookup("concurrency"))
 	config.Viper.BindPFlag("logging.cmds_output", pflags.Lookup("cmds-output"))
 	config.Viper.BindPFlag("security.keyfile", pflags.Lookup("keyfile"))
 	config.Viper.BindPFlag("security.key", pflags.Lookup("key"))
+	config.Viper.BindPFlag("security.encrypted_secrets", pflags.Lookup("encrypted-secrets"))
 
 	rootCmd.AddCommand(
 		NewCompileCommand(config),
